baf309cfda4ea5258942564c286204ec3c711b37
triloksh07 <trilok0726@gmail.com>
2025-07-09 12:57:11 +0530

    changed tagline

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,16 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
+          {/* <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
+          </p> */}
+          {/* Welcome to Acme */}
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
+            <strong>Welcome to Acme.</strong> A full-stack dashboard built using Next.js and NextAuth to explore authentication and protected routes in modern web apps.
           </p>
           <Link
             href="/login"
